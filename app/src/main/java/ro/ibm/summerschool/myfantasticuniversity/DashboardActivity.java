71aa7c874a5fbd313fd2084dd541033291728241
catalinpopica <c_popica@yahoo.com>
2020-07-21 17:56:02 +0300

    Refactoring Dashboard

--- app/src/main/java/ro/ibm/summerschool/myfantasticuniversity/DashboardActivity.java
+++ app/src/main/java/ro/ibm/summerschool/myfantasticuniversity/DashboardActivity.java
@@ -1,18 +1,46 @@
 package ro.ibm.summerschool.myfantasticuniversity;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 
+//import android.widget.Toolbar;
+import com.google.android.material.navigation.NavigationView;
+import androidx.appcompat.widget.Toolbar;
+
+
 public class DashboardActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
 
         CardView cardviewOrar = findViewById(R.id.cardviewOrar);
         cardviewOrar.setOnClickListener(new View.OnClickListener() {
@@ -22,6 +50,16 @@ public class DashboardActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    public void OnBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
 
     private void goToOrar() {
         Intent intent = new Intent(DashboardActivity.this, OrarActivity.class);
